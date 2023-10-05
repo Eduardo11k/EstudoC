6b2a7a5272c351c6fcde35f42956bd2bb5c4d722
Edu Mateus <eduardomateustvs@gmail.com>
2023-10-05 15:58:00 +0100

    Primeiras alterações

---
+++ escopo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+const int x= 11;
+
+
+int main(void){
+printf("x=%i\v",x);
+
+int x=7;
+printf("x=%d\v",x);
+{
+int x=5;
+printf("x=%d\v",x);
+
+}
+printf("x=%i",x);
+}
