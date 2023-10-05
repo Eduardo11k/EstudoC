03c9bc77357b13b8ca4f88739022b71bb603a943
Edu Mateus <eduardomateustvs@gmail.com>
2023-10-05 16:12:32 +0100

    adiconando comentários ao codigo

--- escopo.c
+++ escopo.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include<stdlib.h>
 
-const int x= 11;
+const int x= 11;//criamos um 'const' ou seja uma constante que no caso apartir do momento que declaramos e atribuímos o seu valor já não pode ser alterado
 
 
 int main(void){
-printf("x=%i\v",x);
+printf("x=%i\v",x);//escrevemos na tela a constante coma função printf()e na saida=11
 
 int x=7;
-printf("x=%d\v",x);
+printf("x=%d\v",x);//saida=7
 {
 int x=5;
-printf("x=%d\v",x);
+printf("x=%d\v",x);//saida=5
 
 }
-printf("x=%i",x);
-}
+printf("x=%i",x);//nesse ultimo printf(),qual será a saída ,o que acha tu?fica a seu criterio executar e ver o resultado
+}//e se ainda ouver duvida deixe uma mensagem aí na caixa de questões
