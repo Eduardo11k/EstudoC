7f1a13996c2b284ddf3398c980e42510f438e27a
Edu Mateus <eduardomateustvs@gmail.com>
2023-10-06 13:51:33 +0100

    adicionei comentários as mudança do exerciocios

--- conversoes.c
+++ conversoes.c
@@ -1,12 +1,26 @@
 #include<stdio.h>
-int(){
+#include<stdlib.h>
+int main(){
 
-    int i=120;
-    char ch;
-    double x=90.5;
+    int i=122;//tipo inteiro recebe um valor inteiro ou seja sem parte fracionaria
+    char ch;//tipo caractere recebe uma é usado para armazenar o valor inteiro de um membro do conjunto de caracteres representável,especificado na tabelA ascii 
+    double d=90.5;//tipo recebe um valor real tbm chamado de 'precisão dupla'
 
     ch=i;
-    printf("%c",char);
+    printf("%c\t",ch);// vvalor int foi atribuido a ch tipo char no caso o 'codigo do caractere'que nesta a saída=z 
+
+    i=d;//i inteiro c real 90.5 e por ser inteiro apenas armazenada a 'parte inteira'saida=90
+    printf("%d\t",i);
+
+    d=ch;
+    printf("%f\t",d);// d real recebe o valor char ou seja o codigo do n(122)e o converte em double(real=120.000000)
+
+    
+    d=i;
+    printf("%f\t",d);// d real recebe o valor int (90)e o converte em double(real=90.000000)
+
+
+
 
 return 0;    
 }
