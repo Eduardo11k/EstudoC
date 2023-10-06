d387a0732eb079e75e62b32f318efc44b1ea3815
Edu Mateus <eduardomateustvs@gmail.com>
2023-10-06 13:22:05 +0100

    +mudanças

---
+++ conversoes.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int(){
+
+    int i=120;
+    char ch;
+    double x=90.5;
+
+    ch=i;
+    printf("%c",char);
+
+return 0;    
+}
--- escopo.c
+++ escopo.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 #include<stdlib.h>
+#include<math.h>
 
-const int x= 11;//criamos um 'const' ou seja uma constante que no caso apartir do momento que declaramos e atribuímos o seu valor já não pode ser alterado
+const int x= 11;//criamos um 'const' global ou seja uma constante que no caso apartir do momento que declaramos e atribuímos o seu valor já não pode ser alterado
 
 
 int main(void){
-printf("x=%i\t",x);//escrevemos na tela a constante coma função printf()e na saida=11
+printf("x=%i\t",x);//escrevemos na tela a constante coma função printf()e na saida=11 -varaivel global declarada acima
 
 int x=7;
-printf("x=%d\t",x);//saida=7
+printf("x=%d\t",x);//saida=7 variavel local
 {
 int x=5;
-printf("x=%d\t",x);//saida=5
+printf("x=%d\t",x);//saida=5 variavel local declarada num escopo local dentro de outro local ou seja dentro de 2 escopos locais
+{
 
 }
-printf("x=%i",x);//nesse ultimo printf(),qual será a saída ,o que acha tu?fica a seu criterio executar e ver o resultado
-}//e se ainda ouver duvida deixe uma mensagem aí na caixa de questões
+printf("x=%i",x);/*nesse ultimo printf(),qual será a saída ,o que acha tu?fica a seu criterio executar e ver o resultado
+                 e se ainda ouver duvida deixe uma mensagem aí na caixa de questões*/
+            
+//Agora vamos tocar no aspecto da biblioteca math.h,que é a biblioteca que contem as funções matemáticas(+-/*%,raiz quadrada etc..) muito requeridas hj em dia,principalmente em exames 
+
+
+}
+}
+
